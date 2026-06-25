bcd59787bf08715fcb38cac90852b3741775166a
leooobig <leonardolimastos2003@gmail.com>
2026-06-25 11:54:17 -0300

    Estrutura vazia do projeto

---
+++ src/main/java/com/leooobig/task_time/TaskTimeApplication.java
@@ -0,0 +1,13 @@
+package com.leooobig.task_time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leooobig/task_time/TaskTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leooobig.task_time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
